d47e45e69d3c0ea6e653a7fb2326f57dbb871f51
Nigar Aliyeva <125753820+nigaraliyevas@users.noreply.github.com>
2023-11-12 15:10:11 +0000

    refac:mistakes have been corrected

--- Access-Modifiers-Encapsulation/Program.cs
+++ Access-Modifiers-Encapsulation/Program.cs
@@ -5,7 +5,7 @@ car1.Name = "Carr";//<= instance-da el chatandi*/
 #endregion
 #region protected
 /*Car car2 = new("Car2","Model1",2021);
-car2.Name = "Carr";//<= instance-da el chatandi*/
+car2.Model = "Carr";//<= instance-da el chatan deyil*/
 #endregion
 #region private
 //yalniz class daxilinde el chatandi, inheritance ve instance da el chatan deyil
